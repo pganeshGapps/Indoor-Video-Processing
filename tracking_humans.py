811661fa0c8bc7118e7c8c904d075bd60dd66f19
Varun Kumar <varunkumar@Varuns-MacBook-Pro.local>
2016-09-20 21:38:03 +0530

    added tracking_humans.py

---
+++ tracking_humans.py
@@ -0,0 +1,59 @@
+# USAGE
+# python object_movement.py --video object_tracking_example.mp4
+# python object_movement.py
+
+# import the necessary packages
+from collections import deque
+import numpy as np
+import argparse
+import imutils
+import cv2
+
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-v", "--video",
+	help="path to the (optional) video file")
+args = vars(ap.parse_args())
+
+# initialize the HOG descriptor/person detector
+hog = cv2.HOGDescriptor()
+hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+
+
+# if a video path was not supplied, grab the reference
+# to the webcam
+if not args.get("video", False):
+	camera = cv2.VideoCapture(0)
+
+# otherwise, grab a reference to the video file
+else:
+	camera = cv2.VideoCapture(args["video"])
+
+# keep looping
+while True:
+	# grab the current frame
+	(grabbed, frame) = camera.read()
+
+	# if we are viewing a video and we did not grab a frame,
+	# then we have reached the end of the video
+	if args.get("video") and not grabbed:
+		break
+
+	frame = imutils.resize(frame, width=500)
+
+	# detect people in the frame
+	(rects, weights) = hog.detectMultiScale(frame, winStride=(4, 4), padding=(8, 8), scale=1.05)
+    
+    # draw the original bounding boxes
+	for (x, y, w, h) in rects:
+		cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+		
+	cv2.imshow("Human Tracking",frame)
+	key = cv2.waitKey(1) & 0xFF
+	# if the 'q' key is pressed, stop the loop
+	if key == ord("q"):
+		break
+
+# cleanup the camera and close any open windows
+camera.release()
+cv2.destroyAllWindows()
